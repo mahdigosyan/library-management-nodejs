e60a0b6a8a216f4cabb17d4ea7cabf7ece17a656
Mahdi gosyan <mahdigosyanapple@gmail.com>
2025-08-18 23:37:17 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,83 +1,83 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url')
-const db = require("./db.json");
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url')
+// const db = require("./db.json");
 
-console.log(db);
+// console.log(db);
 
-const server = http.createServer((req,res)=>{
-    if(req.method === "GET" && req.url==="/api/user"){
-        fs.readFile('db.json',(err,db)=>{
-            if(err){
-                throw err
-            }
-            data= JSON.parse(db)
-            res.writeHead(200,{'content-type':'application/json'});
-            res.write(JSON.stringify(data.user))
-            res.end()
-        });
+// const server = http.createServer((req,res)=>{
+//     if(req.method === "GET" && req.url==="/api/user"){
+//         fs.readFile('db.json',(err,db)=>{
+//             if(err){
+//                 throw err
+//             }
+//             data= JSON.parse(db)
+//             res.writeHead(200,{'content-type':'application/json'});
+//             res.write(JSON.stringify(data.user))
+//             res.end()
+//         });
         
             
-    }
-    else if(req.method === "GET" && req.url==="/api/books"){
-        fs.readFile('db.json',(err,db)=>{
-            if(err){
-                throw err
-            }
-            data= JSON.parse(db)
-            res.writeHead(200,{'content-type':'application/json'});
-            res.write(JSON.stringify(data.books))
-            res.end()
-        });
+//     }
+//     else if(req.method === "GET" && req.url==="/api/books"){
+//         fs.readFile('db.json',(err,db)=>{
+//             if(err){
+//                 throw err
+//             }
+//             data= JSON.parse(db)
+//             res.writeHead(200,{'content-type':'application/json'});
+//             res.write(JSON.stringify(data.books))
+//             res.end()
+//         });
         
             
-    }else if(req.method ==="DELETE" && req.url.startsWith("/api/books")){
-        const parsurl = url.parse(req.url,true);
-        const bookId = parsurl.query.id;
-        const newbook = db.books.filter((book) => book.id != bookId)
-        if(newbook.length ===db.books.length){
-            res.writeHead(401,{"content-Type":"application/json"});
-            res.write(JSON.stringify({message:"books not found"}));
-            res.end()
-        }else{
-            fs.writeFile("db.json",JSON.stringify({...db,books:newbook}),(err)=>{
-            if (err){
-                throw err
-            }
-            res.writeHead(200,{'content-type':'application/json'});
-            res.write(JSON.stringify({message :"book remov"}));
-            res.end();
-        }
-    );
-        // res.end("test")
-    }
-    }else if(req.method === "POST" && req.url ==="/api/books"){
-        let book = ""
+//     }else if(req.method ==="DELETE" && req.url.startsWith("/api/books")){
+//         const parsurl = url.parse(req.url,true);
+//         const bookId = parsurl.query.id;
+//         const newbook = db.books.filter((book) => book.id != bookId)
+//         if(newbook.length ===db.books.length){
+//             res.writeHead(401,{"content-Type":"application/json"});
+//             res.write(JSON.stringify({message:"books not found"}));
+//             res.end()
+//         }else{
+//             fs.writeFile("db.json",JSON.stringify({...db,books:newbook}),(err)=>{
+//             if (err){
+//                 throw err
+//             }
+//             res.writeHead(200,{'content-type':'application/json'});
+//             res.write(JSON.stringify({message :"book remov"}));
+//             res.end();
+//         }
+//     );
+//         // res.end("test")
+//     }
+//     }else if(req.method === "POST" && req.url ==="/api/books"){
+//         let book = ""
 
-        req.on("data",(data)=>{
-            book = book + data.toString();
-        });
-        req.on("end",()=>{
-            const newBook = {id: crypto.randomUUID(),...JSON.parse(book),free:1};
-            db.books.push(newBook);
-            fs.writeFile("db.json",JSON.stringify(db),(err)=>{
-                if(err){
-                    throw err
-                }
-            res.writeHead(200,{'content-type':'application/json'});
-            res.write(JSON.stringify({message :"book add anjam shod"}));
-            res.end();
+//         req.on("data",(data)=>{
+//             book = book + data.toString();
+//         });
+//         req.on("end",()=>{
+//             const newBook = {id: crypto.randomUUID(),...JSON.parse(book),free:1};
+//             db.books.push(newBook);
+//             fs.writeFile("db.json",JSON.stringify(db),(err)=>{
+//                 if(err){
+//                     throw err
+//                 }
+//             res.writeHead(200,{'content-type':'application/json'});
+//             res.write(JSON.stringify({message :"book add anjam shod"}));
+//             res.end();
                  
-            })
+//             })
 
 
-            res.end("add new book to db")
-        })
-    }
+//             res.end("add new book to db")
+//         })
+//     }
         
     
-});
+// });
 
-server.listen(3000,()=>{
-    console.log("ow you are running in 3000 port");
-})
+// server.listen(3000,()=>{
+//     console.log("ow you are running in 3000 port");
+// })
